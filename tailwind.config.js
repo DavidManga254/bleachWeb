9c8758d2550adf906d9066129f71499c5be9225f
David Manga <93255852+DavidManga254@users.noreply.github.com>
2023-06-04 23:41:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    screens:{
+      'sm':'10px',
+      'lg':'1024px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
